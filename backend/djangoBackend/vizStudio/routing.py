c3cc2e79b2ab92ee1697b9d3c6946b527d3cafe9
guzman109 <guzman109>
2024-08-30 16:49:21 -0400

    adding a simple healthcheck

--- backend/djangoBackend/vizStudio/routing.py
+++ backend/djangoBackend/vizStudio/routing.py
@@ -1,6 +1,6 @@
 from django.urls import path
 # from .views import calculate_length, text2query, page_method, work_method, audio_main
-from .views import calculate_length,text2query, wake_and_asr,sparql_execute, generate_vega
+from .views import calculate_length,text2query, wake_and_asr,sparql_execute, generate_vega, healthcheck
 from .consumers import QueryConsumer
 
 
@@ -12,7 +12,8 @@ urlpatterns = [
     path('calculate_length/', calculate_length, name='calculate_length'),
     path('wake_and_asr/', wake_and_asr, name='wake_and_asr'),
     path('sparql_execute/', sparql_execute, name = 'sparql_execute'),
-    path('generate_vega/', generate_vega, name = 'generate_vega')
+    path('generate_vega/', generate_vega, name = 'generate_vega'),
+    path('healthcheck/', healthcheck, name='healthcheck')
 ]
 websocket_urlpatterns = [
     path('ws/query/', QueryConsumer.as_asgi()),
