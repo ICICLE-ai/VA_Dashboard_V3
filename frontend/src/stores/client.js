c22df308dc266c8ed1e1512bc52ab5e1ac695ebd
Zeyu Yuan <64967931+yzy1121@users.noreply.github.com>
2024-11-26 14:29:23 -0500

    Update client.js

--- frontend/src/stores/client.js
+++ frontend/src/stores/client.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 export const client = axios.create({
-    baseURL: import.meta.env.VITE_APP_BACKEND_BASE_URL || "__BACKEND_BASE_URL__",
+    baseURL: import.meta.env.VA_BACKEND_URL || "__BACKEND_BASE_URL__",
     // timeout: 1000,
 });
