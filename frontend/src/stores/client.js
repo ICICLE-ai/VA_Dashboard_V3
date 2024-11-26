e12b063fe787ec79f896512ef94aa295c6046d28
Zeyu Yuan <64967931+yzy1121@users.noreply.github.com>
2024-11-26 14:46:30 -0500

    Update client.js

--- frontend/src/stores/client.js
+++ frontend/src/stores/client.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 export const client = axios.create({
-    baseURL: import.meta.env.VA_BACKEND_URL || "__BACKEND_BASE_URL__",
+    baseURL: import.meta.env.VA_BACKEND_URL || "pods-tacc-icicleai-vadashback",
     // timeout: 1000,
 });
